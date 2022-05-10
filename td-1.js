b5a36fa7cc44f8bd089655443c7ade49153f9636
Mael <mtcarron@gmail.com>
2022-05-10 08:53:59 +0200

    modification de td-1.js

--- td-1.js
+++ td-1.js
@@ -1,3 +1,19 @@
+const txtToAdd = document.querySelector('#txt_to_add');
+const pList = document.querySelector('#myList p');
+const pArray = document.querySelector('#myArray p');
+const myArray = ['une ligne', 'autre chose', 'et encore'];
+
 function addText(){
-    document.querySelector('#myList p').innerText = document.getElementById('txt_to_add').value;
+    if(txtToAdd.value!=='')
+    {
+        // Ajout du contenu du input dans l'élément p.
+        pList.innerText += txtToAdd.value+'\n';
+        // Plist.innerText = pList.innerText + txtToAdd.value+'\n';
+        txtToAdd.value = '';
+    }
+    else{
+        alert('Veuillez entrer un texte');
+    }
+    txtToAdd.focus();
 }
+// document.querySelector('#myList p').innerText = document.getElementById('txt_to_add').value;
